47626b48bc00fb7dc710197a54496f42c9a258f1
Rasel91 <srasel.dpe@gmail.com>
2022-10-07 22:15:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This is tailwindcss</h1>
+     <p>This is tailwindcss</p>
+    </div>
+  );
+}
+
+export default App;
